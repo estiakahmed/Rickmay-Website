c5ee1493d3f8a4e7ea0744c7acb3afbb61ec2c32
estiakahmed <71464257+estiakahmed@users.noreply.github.com>
2021-08-28 19:21:57 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.getElementById("firstcheckbbtn").onclick = function () {
+    location.href = "https://financialsupportonline.co.uk/form/?firstName=Riccardo&lastName=May&phone=07777777778&email=riccardo@monetise.co.uk&source=new-source&clickid=123456";
+};
+document.getElementById("secondcheckbtn").onclick = function () {
+    location.href = "https://financialsupportonline.co.uk/form/?firstName=Riccardo&lastName=May&phone=07777777778&email=riccardo@monetise.co.uk&source=new-source&clickid=123456";
+};
+document.getElementById("peacecheck").onclick = function () {
+    location.href = "https://financialsupportonline.co.uk/form/?firstName=Riccardo&lastName=May&phone=07777777778&email=riccardo@monetise.co.uk&source=new-source&clickid=123456";
+};
+document.getElementById("fourthbtn").onclick = function () {
+    location.href = "https://financialsupportonline.co.uk/form/?firstName=Riccardo&lastName=May&phone=07777777778&email=riccardo@monetise.co.uk&source=new-source&clickid=123456";
+};
+document.getElementById("financialbutton").onclick = function () {
+    location.href = "https://financialsupportonline.co.uk/form/?firstName=Riccardo&lastName=May&phone=07777777778&email=riccardo@monetise.co.uk&source=new-source&clickid=123456";
+};
+
